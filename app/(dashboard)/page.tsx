4b9fb36e2d829b6e2583ff9e358eda62b5aca77a
DarthCoder501 <peguerojoshua67@gmail.com>
2024-10-26 18:56:48 -0400

    Fixed friend request issue

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { AddFriend } from "./_components/add-friend";
 import {
   AcceptedFriendsList,
   PendingFriendsList,
@@ -12,7 +12,7 @@ export default function FriendsPage() {
     <div className="flex-1 flex-col flex divide-y">
       <header className="flex items-center justify-between p-4">
         <h1 className="font-semibold">Friends</h1>
-        <Button size="sm">Add Friend</Button>
+        <AddFriend />
       </header>
       <div className="grid p-4 gap-4"></div>
       <TooltipProvider delayDuration={0}>
