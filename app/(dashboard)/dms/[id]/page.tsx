a55e5a0ba44816b53312f3270245d41876162ba5
DarthCoder501 <peguerojoshua67@gmail.com>
2024-10-29 13:18:40 -0400

    Debuged 404 page error

--- app/(dashboard)/dms/[id]/page.tsx
+++ app/(dashboard)/dms/[id]/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { SendIcon, MoreVerticalIcon, TrashIcon, PlusIcon } from "lucide-react";
